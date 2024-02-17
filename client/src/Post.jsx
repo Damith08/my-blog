989651f309297c3f1ac0615d3af6b27ca18ba8b0
Damith08 <dmthsamarasinghe@gmail.com>
2024-02-17 11:25:23 +0530

    feat: when click on the image and the title it will route to the post

--- client/src/Post.jsx
+++ client/src/Post.jsx
@@ -1,13 +1,19 @@
 import { format } from "date-fns";
+import { Link } from "react-router-dom";
 
-function Post({ title, summary, cover, content, createdAt, author }) {
+function Post({ _id, title, summary, cover, content, createdAt, author }) {
   return (
     <div className="post">
       <div className="img">
-        <img src={"http://localhost:4000/" + cover} alt="post-image" />
+        <Link to={`/post/${_id}`}>
+          <img src={"http://localhost:4000/" + cover} alt="post-image" />
+        </Link>
       </div>
       <div className="texts">
-        <h2>{title}</h2>
+        <Link to={`/post/${_id}`}>
+          <h2>{title}</h2>
+        </Link>
+
         <p className="info">
           <a href="" className="author">
             {author.username}
